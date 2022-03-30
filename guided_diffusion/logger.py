971edb5316e55fb8d2dfd92bb2f9ebb04071984d
zbirobin <zbi.robin@gmail.com>
2022-03-30 16:52:00 +0200

    Add TensorBoard to logger.

--- guided_diffusion/logger.py
+++ guided_diffusion/logger.py
@@ -15,6 +15,8 @@ import warnings
 from collections import defaultdict
 from contextlib import contextmanager
 
+from torch.utils.tensorboard import SummaryWriter
+
 DEBUG = 10
 INFO = 20
 WARN = 30
@@ -155,32 +157,12 @@ class TensorBoardOutputFormat(KVWriter):
     def __init__(self, dir):
         os.makedirs(dir, exist_ok=True)
         self.dir = dir
-        self.step = 1
-        prefix = "events"
-        path = osp.join(osp.abspath(dir), prefix)
-        import tensorflow as tf
-        from tensorflow.python import pywrap_tensorflow
-        from tensorflow.core.util import event_pb2
-        from tensorflow.python.util import compat
-
-        self.tf = tf
-        self.event_pb2 = event_pb2
-        self.pywrap_tensorflow = pywrap_tensorflow
-        self.writer = pywrap_tensorflow.EventsWriter(compat.as_bytes(path))
+        self.writer = SummaryWriter("log/runs/")
 
     def writekvs(self, kvs):
-        def summary_val(k, v):
-            kwargs = {"tag": k, "simple_value": float(v)}
-            return self.tf.Summary.Value(**kwargs)
-
-        summary = self.tf.Summary(value=[summary_val(k, v) for k, v in kvs.items()])
-        event = self.event_pb2.Event(wall_time=time.time(), summary=summary)
-        event.step = (
-            self.step
-        )  # is there any reason why you'd want to specify the step?
-        self.writer.WriteEvent(event)
-        self.writer.Flush()
-        self.step += 1
+        for k, v in kvs.items():
+            self.writer.add_scalar(tag=k, scalar_value=float(v))
+        self.writer.flush()
 
     def close(self):
         if self.writer:
