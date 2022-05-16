69f8fa7529f4f73a82c702479e44c1fa8d2d1b09
zbirobin <zbi.robin@gmail.com>
2022-05-16 11:17:26 +0200

    Refactor train_prior and add early stopping.

--- robin_scripts/train_prior.py
+++ robin_scripts/train_prior.py
@@ -12,6 +12,7 @@ from guided_diffusion import dist_util, logger
 from guided_diffusion.script_util import (
     add_dict_to_argparser
 )
+from guided_diffusion.mlp import MLP_mixer
 
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
@@ -38,70 +39,6 @@ class CocoDataset(Dataset):
         img_embedding = self.img_embeddings[ind]
         img_embedding = (img_embedding - self.img_mean)/self.img_std
         return th.from_numpy(txt_embedding).float(), th.from_numpy(img_embedding).float()
-
-
-class MLP(nn.Module):
-
-    def __init__(self, emb_dim, width, num_layers, dropout=0.0):
-        super(MLP, self).__init__()
-
-        layers = []
-
-        layers.append(nn.Linear(emb_dim, width))
-        layers.append(nn.SiLU())
-        
-        for _ in range(num_layers):
-            layers.append(nn.BatchNorm1d(width))
-            layers.append(nn.Linear(width, width))
-            layers.append(nn.SiLU())
-            layers.append(nn.Dropout(p=dropout))
-    
-        layers.append(nn.Linear(width, emb_dim))
-
-        self.layers = nn.Sequential(*layers)
-
-    def forward(self, x):
-        for layer in self.layers:
-            x = layer(x) + x
-        return x
-
-
-class MLP_mixer(nn.Module):
-
-    def __init__(self, emb_dim, width, num_layers, dropout=0.1):
-        super(MLP_mixer, self).__init__()
-
-        class PreNormResidual(nn.Module):
-            def __init__(self, dim, fn):
-                super().__init__()
-                self.fn = fn
-                self.norm = nn.LayerNorm(dim)
-
-            def forward(self, x):
-                return self.fn(self.norm(x)) + x
-
-        def FeedForward(dim, expansion_factor=4, dropout=0.0, dense=nn.Linear):
-            inner_dim = int(dim * expansion_factor)
-            return nn.Sequential(
-                dense(dim, inner_dim),
-                nn.GELU(),
-                nn.Dropout(dropout),
-                dense(inner_dim, dim),
-                nn.Dropout(dropout)
-            )
-
-        self.layers = nn.Sequential(
-            nn.Linear(emb_dim, width),
-            *[
-                PreNormResidual(width, FeedForward(width, 2, dropout)) for _ in range(num_layers)],
-            nn.LayerNorm(width),
-            nn.Linear(width, emb_dim)
-        )
-
-    def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
-        return x
 
 
 def main():
@@ -166,7 +103,7 @@ def main():
                     loss = criterion(output, val_img_emb.to(device))
                     logger.logkv_mean("validation loss", loss.item())
                     if loss < best_val_loss:
-                        th.save(model.state_dict(), f"{args.model_name}/model_ES{(iteration):08d}.pt")
+                        th.save(model.state_dict(), f"{dir}/model_ES.pt")
 
                 # Identity model loss
                 loss = criterion(val_txt_emb.to(device), val_img_emb.to(device))
@@ -175,8 +112,8 @@ def main():
                 logger.dumpkvs()
 
             if iteration % args.save_interval == 0:
-                th.save(model.state_dict(), f"{args.model_name}/model{(iteration):08d}.pt")
-                th.save(optimizer.state_dict(), f"{args.model_name}/opt{(iteration):08d}.pt")
+                th.save(model.state_dict(), f"{dir}/model{(iteration):08d}.pt")
+                th.save(optimizer.state_dict(), f"{dir}/opt{(iteration):08d}.pt")
 
             iteration += 1
 
@@ -185,11 +122,11 @@ def create_argparser():
     defaults = dict(
         model_name="model",
         log_interval=1000,
-        save_interval=100000,
-        batch_size=64,
-        weight_decay=0.01,
-        dropout=0.1,
-        epochs = 10000,
+        save_interval=10000,
+        batch_size=256,
+        weight_decay=0.04,
+        dropout=0.3,
+        epochs=1000,
         emb_dim=512
     )
     parser = argparse.ArgumentParser()
