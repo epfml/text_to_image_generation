d88bff7a7942d34b06ba2a1ac6d93c50f67fe808
zbirobin <zbi.robin@gmail.com>
2022-06-10 16:23:38 +0200

    Rename prior to translator

--- robin_scripts/shell_files/train_translator.sh
+++ robin_scripts/shell_files/train_translator.sh
@@ -1,14 +1,14 @@
 cd ..
 
 python train_prior.py \
-    --model_name "final_6" \
+    --model_name "final_7" \
     --log_interval 50 \
     --save_interval 200000 \
     --batch_size 256 \
-    --weight_decay 0.0001 \
+    --weight_decay 0.0002 \
     --dropout 0.1 \
     --epochs 100 \
     --emb_dim 512 \
-    --num_layers 20 \
+    --num_layers 30 \
 
 cd shell_files
