e53f2ba1b0c7189366911c02eeddaaf03f175245
zbirobin <zbi.robin@gmail.com>
2022-07-07 23:08:04 +0200

    Refactor and cleaning dataset_helpers.py

--- guided_diffusion/dataset_helpers.py
+++ guided_diffusion/dataset_helpers.py
@@ -7,14 +7,37 @@ import torch as th
 
 from torch.utils.data import Dataset
 
+
+# Embeddings data paths
 EMBEDDING_IMAGE_MEAN_PATH = "../../../../mlodata1/roazbind/embeddings_stats/image_embedding_mean.npy"
 EMBEDDING_IMAGE_STD_PATH = "../../../../mlodata1/roazbind/embeddings_stats/image_embedding_std.npy"
 EMBEDDING_CAPTION_MEAN_PATH = "../../../../mlodata1/roazbind/embeddings_stats/caption_embedding_mean.npy"
 EMBEDDING_CAPTION_STD_PATH = "../../../../mlodata1/roazbind/embeddings_stats/caption_embedding_std.npy"
 
+EMBEDDING_IMAGE_COCOVAL_PATH = "../../../../mlodata1/roazbind/coco/val_embedding_img.npy"
+EMBEDDING_IMAGE_IMAGENET_BATCH_PATH = lambda i : \
+                f"../../../../mlodata1/roazbind/imagenet64/embedding_batch_{i}.npy"
+
+# Images data paths
+IMAGES_IMAGENET_BATCH_PATH = lambda i : \
+                f"../../../../mlodata1/roazbind/imagenet64/train_data_batch_{i}.npy"
+IMAGES_CC_PATH = lambda dataset_name, image_size, i : \
+                f"../../../../mlodata1/roazbind/{dataset_name}/images_{image_size}/0{str(i).zfill(4)}.tar"
+
+
+# --------- Datasets of images ---------
 
 class ImagenetDataset(Dataset):
 
+    """
+    Create the ImageNet dataset.
+
+    :param transform: transformation applied to each image.
+    :param normalize_emb: if True, the embeddings are normalized.
+    :param drop_emb_proba: probability to drop the embedding, i.e., to
+                            set the embedding to the null vector.
+    :return: the ImageNet dataset.
+    """
     def __init__(self, transform=None, normalize_emb=True, drop_emb_proba=0.2):
 
         if normalize_emb:
@@ -24,8 +47,8 @@ class ImagenetDataset(Dataset):
         self.transform = transform
         self.drop_emb_proba = drop_emb_proba
 
-        img_paths = [f"../../../../mlodata1/roazbind/imagenet64/train_data_batch_{i}.npy" for i in range(1, 11)]
-        emb_paths = [f"../../../../mlodata1/roazbind/imagenet64/embedding_batch_{i}.npy" for i in range(1, 11)]
+        img_paths = [IMAGES_IMAGENET_BATCH_PATH(i) for i in range(1, 11)]
+        emb_paths = [EMBEDDING_IMAGE_IMAGENET_BATCH_PATH(i) for i in range(1, 11)]
 
         self.img_memmaps = [np.load(path, mmap_mode='r') for path in img_paths]
         self.emb_memmaps = [np.load(path, mmap_mode='r') for path in emb_paths]
@@ -40,6 +63,7 @@ class ImagenetDataset(Dataset):
         return self.img_count
 
     def __getitem__(self, index):
+        """Get the image and its corresponding embedding of the specified index."""
 
         memmap_index = bisect(self.start_indices, index) - 1
         index_in_memmap = index - self.start_indices[memmap_index]
@@ -62,6 +86,19 @@ class ImagenetDataset(Dataset):
 
 class CCDataset(Dataset):
 
+    """
+    Create a Conceptual Captions (CC) dataset by only considering the images
+    and not the captions.
+
+    :param num_shard: number of shards used to store the dataset.
+    :param image_folder_path: path of the folder containing the images.
+    :param embeddings_folder_path: path of the folder containing the image embeddings.
+    :param transform: transformation applied to each image.
+    :param normalize_emb: if True, the embeddings are normalized.
+    :param drop_emb_proba: probability to drop the embedding, i.e., to
+                            set the embedding to the null vector.
+    :return: a Conceptual Captions (CC) dataset.
+    """
     def __init__(self, num_shard, image_folder_path, embeddings_folder_path, transform=None, normalize_emb=True, drop_emb_proba=0.2):
 
         if normalize_emb:
@@ -87,6 +124,7 @@ class CCDataset(Dataset):
         return self.img_count
 
     def __getitem__(self, index):
+        """Get the image and its corresponding embedding of the specified index."""
 
         memmap_index = bisect(self.start_indices, index) - 1
         index_in_memmap = index - self.start_indices[memmap_index]
@@ -107,8 +145,19 @@ class CCDataset(Dataset):
         return img, emb
 
 
+# --------- Datasets of embeddings pairs ---------
+
 class CCCaptionsDataset(Dataset):
 
+    """
+    Create a Conceptual Captions (CC) dataset by considering the image
+    and text embeddings only.
+
+    :param num_shard: number of shards used to store the dataset.
+    :param img_emb_folder_path: path of the folder containing the image embeddings.
+    :param txt_emb_folder_path: path of the folder containing the text embeddings.
+    :return: a Conceptual Captions (CC) dataset.
+    """
     def __init__(self, num_shard, img_emb_folder_path, txt_emb_folder_path):
 
         
@@ -133,6 +182,7 @@ class CCCaptionsDataset(Dataset):
         return self.emb_count
 
     def __getitem__(self, index):
+        """Get the image and text embedding of the specified index."""
 
         memmap_index = bisect(self.start_indices, index) - 1
         index_in_memmap = index - self.start_indices[memmap_index]
@@ -150,6 +200,14 @@ class CCCaptionsDataset(Dataset):
 
 class CocoDataset(Dataset):
 
+    """
+    Create the MS-COCO dataset by considering the image
+    and text embeddings only.
+
+    :param txt_embeddings: numpy array containing the text embeddings.
+    :param img_embeddings: numpy array containing the image embeddings.
+    :return: the MS-COCO dataset
+    """
     def __init__(self, txt_embeddings, img_embeddings):
         self.num_captions = 5
         self.txt_embeddings = txt_embeddings
@@ -164,6 +222,8 @@ class CocoDataset(Dataset):
         return self.img_embeddings.shape[0]
     
     def __getitem__(self, ind):
+        """Get the image and text embedding of the specified index."""
+
         # Get a random caption among the available ones
         txt_embedding = self.txt_embeddings[ind][np.random.randint(self.num_captions)]
         txt_embedding = (txt_embedding - self.txt_emb_mean)/self.txt_emb_std
@@ -173,13 +233,15 @@ class CocoDataset(Dataset):
 
 
 def get_iterator(dataloader):
-        while True:
-            yield from dataloader
+    """Create an iterator from the dataloader."""
+    while True:
+        yield from dataloader
 
 
 def resize_images(dataset, num_shard, batch_size=512):
     """
-    Resize images of dataset from size 256x256 to 64x64.
+    Resize images of dataset from size 256x256 to 64x64 for the 
+    Conceptual Captions (cc3m and cc12m) datasets.
     """
 
     if dataset not in ["cc3m", "cc12m"]:
@@ -188,7 +250,7 @@ def resize_images(dataset, num_shard, batch_size=512):
     with th.no_grad():
         for i in range(1, num_shard + 1):
             print(f"shard {i}")
-            url = f"../../../../mlodata1/roazbind/{dataset}/images_256/0{str(i).zfill(4)}.tar"
+            url = IMAGES_CC_PATH(dataset_name=dataset, image_size=256, i=i)
             
             dataset = wds.WebDataset(url).decode("pil").to_tuple("jpg;png")
 
@@ -199,4 +261,4 @@ def resize_images(dataset, num_shard, batch_size=512):
                 resized.append([np.array(image.resize((64, 64))) for image in np.squeeze(images)])
 
             resized = np.concatenate(resized, axis=0)
-            np.save(f"../../../../mlodata1/roazbind/{dataset}/images_64/0{str(i).zfill(4)}.npy", resized)
+            np.save(IMAGES_CC_PATH(dataset_name=dataset, image_size=64, i=i), resized)
